95b624df82ce1fb6179fb65731a109e7de362296
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-03-29 10:58:36 +0200

    front pages  and corresponding routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignUp'
+
+
 export default function App() {
   return (
-    <h2 className="text-3xl font-bold underline">
-      Hello world!
-      console.log('this is a comment');
-    </h2>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-out" element={<SignOut />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
