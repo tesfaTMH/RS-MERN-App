8fb0b8039b7e02271830dc59bff87916c5b095f6
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-03-29 11:51:19 +0200

    Header component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,13 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignOut from './pages/SignUp'
 
+import Header from "./components/Header"
+
 
 export default function App() {
   return (
    <BrowserRouter>
+   <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
