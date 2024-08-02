ba7b0ccfab78f4f4af31068b6512383843bf3348
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-08-02 16:39:10 +0300

    Backend and frontend signin and signup functionality added using google OAuth with google firebase

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,29 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaWPe04ve7gxfEdYYJSI54lNBlQID_tO0bAw&s",
     },
     isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
-    }
-}, 
-{timestamps: true}
-)
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   signinController,
   signupController,
+  googleController,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/signin", signinController);
+router.post("/google", googleController);
 
 export default router;
