c18cd6f877be54c898eb48ce9e81efd6ec1471af
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-08-02 14:59:05 +0300

    Frontend redux-persist added to keep global state after page refresh

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// use root reducer to combine multiple reducer
+const rootReducer = combineReducers({ user: userReducer });
+
+// persistConfig defines the key, version, name of local storage
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// create a persist reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleWare) =>
     getDefaultMiddleWare({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
