184507d9d65cd5b6895f09c583df646f812aca47
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-08-08 21:53:53 +0300

    minor modification

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -94,9 +94,11 @@ const Listing = () => {
               )}
               <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
                 <p className="text-2xl font-semibold">
-                  {listing.name} -{" "}
+                  {listing.name} - $
                   {listing.offer
-                    ? listing.discountPrice.toLocaleString("en-US")
+                    ? (
+                        +listing.regularPrice - +listing.discountPrice
+                      ).toLocaleString("en-US")
                     : listing.regularPrice.toLocaleString("en-US")}
                   {listing.type === "rent" && " / month"}
                 </p>
@@ -110,7 +112,7 @@ const Listing = () => {
                   </p>
                   {listing.offer && (
                     <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-2 rounded-md">
-                      ${+listing.regularPrice - +listing.discountPrice} OFF
+                      ${listing.discountPrice} OFF
                     </p>
                   )}
                 </div>
